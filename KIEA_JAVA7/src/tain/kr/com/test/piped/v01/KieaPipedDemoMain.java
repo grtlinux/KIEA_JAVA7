9a3c3d412311b42f2a984b99e894449f9f52b47a
Kiea Seok Kang <grtlinux@gmail.com>
2016-08-09 09:17:07 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/piped/v01/KieaPipedDemoMain.java
+++ KIEA_JAVA7/src/tain/kr/com/test/piped/v01/KieaPipedDemoMain.java
@@ -19,6 +19,11 @@
  */
 package tain.kr.com.test.piped.v01;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -42,15 +47,123 @@ public class KieaPipedDemoMain {
 	private static final Logger log = Logger.getLogger(KieaPipedDemoMain.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static PipedInputStream pis = null;
+	private static PipedOutputStream pos = null;
+
+	//private static InputStream is = null;
+	//private static OutputStream os = null;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	
 	private static void test01(String[] args) throws Exception {
 		
 		if (flag) {
+			pis = new PipedInputStream();
+			pos = new PipedOutputStream(pis);
+		}
+		
+		if (!flag) {
 			
+			System.out.println("Thread-1");
+			
+			Thread thr1 = new Thread() {
+				@Override
+				public void run() {
+					try {
+						for (int i=1; i < 51; i++) {
+							pos.write(i);
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			};
+			
+			thr1.start();
+			thr1.join();
+			
+			while (pis.available() > 0) {
+				int val = pis.read();
+				System.out.println("> " + val);
+			}
+			
+			System.out.println("Finish");
+		}
+		
+		if (!flag) {
+			
+			System.out.println("Thread-1");
+			
+			Thread thr1 = new Thread() {
+				@Override
+				public void run() {
+					try {
+						for (int i=1; i < 1000; i++) {  // TODO 2016.08.08 : ERROR in Piped stream
+							pos.write(i);
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			};
+			
+			thr1.start();
+
+			try {
+				int val = 0;
+				
+				while (true) {
+					val = pis.read();
+					System.out.println("> " + val);
+				}
+			} catch (Exception e) {
+				//e.printStackTrace();
+			}
+			
+			System.out.println("Finish");
+		}
+		
+		if (flag) {
+			
+			System.out.println("Thread-1");
+			
+			Thread thr1 = new Thread() {
+				@Override
+				public void run() {
+					
+					DataOutputStream dos = new DataOutputStream(pos);
+					
+					try {
+						for (int i=1; i < 1000000; i++) {
+							dos.writeInt(i);
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			};
+			
+			thr1.start();
+
+			try {
+				
+				DataInputStream dis = new DataInputStream(pis);
+				
+				int val = 0;
+				
+				while (true) {
+					val = dis.readInt();
+					System.out.println("> " + val);
+				}
+			} catch (Exception e) {
+				//e.printStackTrace();
+			}
+			
+			System.out.println("Finish");
 		}
 	}
-	
+
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	public static void main(String[] args) throws Exception {
