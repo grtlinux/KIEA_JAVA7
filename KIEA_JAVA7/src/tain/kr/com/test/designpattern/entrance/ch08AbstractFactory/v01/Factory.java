c9a7d23fe992b73dab4f7c8eb690aa64b2d368e6
Kiea Seok Kang <grtlinux@gmail.com>
2016-09-07 16:52:41 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch08AbstractFactory/v01/Factory.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.designpattern.entrance.ch08AbstractFactory.v01;
+
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : Factory.java
+ *   -. Package    : tain.kr.com.test.designpattern.entrance.ch08AbstractFactory.v01
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 9. 7. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public abstract class Factory {
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public static Factory getFactory(String className) {
+		Factory factory = null;
+		
+		try {
+			factory = (Factory) Class.forName(className).newInstance();
+		} catch (ClassNotFoundException e) {
+			System.out.println("클래스 " + className + "이 발견되지 않습니다.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return factory;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public abstract ItemLink createLink(String caption, String url);
+	public abstract ItemTray createTray(String caption);
+	public abstract ItemPage createPage(String title, String author);
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+}
