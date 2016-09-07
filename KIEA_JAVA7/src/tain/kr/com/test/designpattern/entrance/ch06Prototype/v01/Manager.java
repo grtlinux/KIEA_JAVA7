c8900755ade730e64764c2b928d7963292fb217a
Kiea Seok Kang <grtlinux@gmail.com>
2016-09-07 14:12:38 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch06Prototype/v01/Manager.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.designpattern.entrance.ch06Prototype.v01;
+
+import java.util.Hashtable;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : Manager.java
+ *   -. Package    : tain.kr.com.test.designpattern.entrance.ch06Prototype.v01
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 9. 7. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class Manager {
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private final Hashtable<String, Product> showCase;
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public Manager() {
+		this.showCase = new Hashtable<String, Product>();
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public void register(String name, Product proto) {
+		this.showCase.put(name, proto);
+	}
+	
+	public Product create(String protoName) {
+		Product product = (Product) this.showCase.get(protoName);
+		return product.createClone();
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+}
