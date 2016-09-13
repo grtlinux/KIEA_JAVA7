3041c171fd229975d0ae0130091360c96ccbc8d0
Kiea Seok Kang <grtlinux@gmail.com>
2016-09-14 07:35:11 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch12Decorator/v01/FullBorder.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.designpattern.entrance.ch12Decorator.v01;
+
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : FullBorder.java
+ *   -. Package    : tain.kr.com.test.designpattern.entrance.ch12Decorator.v01
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 9. 14. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class FullBorder extends Border {
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public FullBorder(Display display) {
+		super(display);
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Override
+	public int getColumns() {
+		return 1 + super.getDisplay().getColumns() + 1;
+	}
+	
+	@Override
+	public int getRows() {
+		return 1 + super.getDisplay().getRows() + 1;
+	}
+	
+	@Override
+	public String getRowText(int row) {
+		
+		if (row == 0) {
+			return "+" + makeLine('-', super.getDisplay().getColumns()) + "+";
+		} else if (row == super.getDisplay().getRows() + 1) {
+			return "+" + makeLine('-', super.getDisplay().getColumns()) + "+";
+		} else {
+			return "|" + super.getDisplay().getRowText(row - 1) + "|";
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private String makeLine(char ch, int count) {
+		
+		StringBuffer sb = new StringBuffer();
+		
+		for (int i=0; i < count; i++) {
+			sb.append(ch);
+		}
+		
+		return sb.toString();
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+}
