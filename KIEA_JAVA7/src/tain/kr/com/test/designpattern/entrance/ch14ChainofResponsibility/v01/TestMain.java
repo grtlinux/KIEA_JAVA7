1efbe309877b66299f34e623276a2aa0a20d1680
Kiea Seok Kang <grtlinux@gmail.com>
2016-09-14 15:40:20 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch14ChainofResponsibility/v01/TestMain.java
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch14ChainofResponsibility/v01/TestMain.java
@@ -51,7 +51,21 @@ public class TestMain {
 	private static void test01(String[] args) throws Exception {
 		
 		if (flag) {
+			// create Support class
+			Support alice = new NoSupport("Alice");
+			Support bob = new LimitSupport("Bob", 100);
+			Support charlie = new SpecialSupport("Charlie", 429);
+			Support diana = new LimitSupport("Diana", 200);
+			Support elmo = new OddSupport("Elmo");
+			Support fred = new LimitSupport("Fred", 300);
 			
+			// make changes
+			alice.setNext(bob).setNext(charlie).setNext(diana).setNext(elmo).setNext(fred);
+			
+			// trouble event
+			for (int i=0; i < 500; i++) {
+				alice.support(new Trouble(i));
+			}
 		}
 	}
 	
