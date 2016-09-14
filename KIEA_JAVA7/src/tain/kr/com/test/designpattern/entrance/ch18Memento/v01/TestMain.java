38c3c8630170bc9a77ee29ca3f451cfc7f309ede
Kiea Seok Kang <grtlinux@gmail.com>
2016-09-15 00:09:01 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch18Memento/v01/TestMain.java
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch18Memento/v01/TestMain.java
@@ -51,7 +51,30 @@ public class TestMain {
 	private static void test01(String[] args) throws Exception {
 		
 		if (flag) {
+			Gamer gamer = new Gamer(100);
 			
+			Memento memento = gamer.createMemento();
+			
+			for (int i=0; i < 100; i++) {
+				System.out.println("==== " + i);
+				System.out.println("현 상태:" + gamer);
+				
+				gamer.bet();
+				
+				System.out.println("돈은 " + gamer.getMoney() + "원이 되었습니다.");
+				
+				if (gamer.getMoney() > memento.getMoney()) {
+					System.out.println("    (많이 증가했으니 현재의 상태를 보존해두자)");
+					memento = gamer.createMemento();
+				} else if (gamer.getMoney() < memento.getMoney() / 2) {
+					System.out.println("    (많이 줄었으니 이전의 상태로 복귀하자)");
+					gamer.restoreMemento(memento);
+				}
+				
+				try { Thread.sleep(1000); } catch (InterruptedException e) {}
+				
+				System.out.println();
+			}
 		}
 	}
 	
