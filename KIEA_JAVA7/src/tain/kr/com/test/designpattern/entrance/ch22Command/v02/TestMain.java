a427a18078fcc53d205b261f3ddd6c3349ae2331
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-04 20:22:53 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch22Command/v02/TestMain.java
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch22Command/v02/TestMain.java
@@ -19,6 +19,18 @@
  */
 package tain.kr.com.test.designpattern.entrance.ch22Command.v02;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -35,20 +47,98 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
-public class TestMain {
+public class TestMain extends JFrame implements ActionListener, MouseMotionListener, WindowListener {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 
 	private static boolean flag = true;
 
 	private static final Logger log = Logger.getLogger(TestMain.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private final MacroCommand history;
+	private final CanvasDraw canvas;
+	private final JButton clearButton;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	
-	public TestMain() {
+	public TestMain(String title) {
 		
-		if (flag) log.debug(" in class " + this.getClass().getSimpleName());
+		super(title);
+
+		if (flag) log.debug(">>>>> in class " + this.getClass().getSimpleName());
+		
+		// final private member
+		this.history = new MacroCommand();
+		this.canvas = new CanvasDraw(400, 400, this.history);
+		this.clearButton = new JButton("clear");
+		
+		// listener
+		this.addWindowListener(this);
+		this.canvas.addMouseMotionListener(this);
+		this.clearButton.addActionListener(this);
+		
+		// layout
+		Box buttonBox = new Box(BoxLayout.X_AXIS);
+		buttonBox.add(this.clearButton);
+		
+		Box mainBox = new Box(BoxLayout.Y_AXIS);
+		mainBox.add(buttonBox);
+		mainBox.add(this.canvas);
+		
+		getContentPane().add(mainBox);
+		
+		pack();
+		setVisible(true);
 	}
 	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/*
+	 * ActionListener
+	 */
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == this.clearButton) {
+			this.history.clear();
+			this.canvas.repaint();
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/*
+	 * MouseMotionListener
+	 */
+	public void mouseMoved(MouseEvent e) {}
+	
+	public void mouseDragged(MouseEvent e) {
+		CommandImpl command = new DrawCommand(this.canvas, e.getPoint());
+		this.history.append(command);
+		command.execute();
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/*
+	 * WindowListener
+	 */
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);
+	}
+	
+	public void windowOpened(WindowEvent e) {}
+	public void windowClosed(WindowEvent e) {}
+	public void windowActivated(WindowEvent e) {}
+	public void windowDeactivated(WindowEvent e) {}
+	public void windowIconified(WindowEvent e) {}
+	public void windowDeiconified(WindowEvent e) {}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -56,7 +146,7 @@ public class TestMain {
 
 	private static void test01(String[] args) throws Exception {
 		
-		if (flag) new TestMain();
+		if (flag) new TestMain("Command Pattern Sample");
 		
 		if (flag) {
 			
