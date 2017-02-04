74153d7a5b8e84eab7def9cbfef35163476dcf0e
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-05 08:04:46 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch16Mediator/v01/LoginFrame.java
@@ -0,0 +1,160 @@
+/**
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.designpattern.entrance.ch16Mediator.v01;
+
+import java.awt.CheckboxGroup;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : LoginFrame.java
+ *   -. Package    : tain.kr.com.test.designpattern.entrance.ch16Mediator.v01
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2017. 2. 5. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class LoginFrame extends Frame implements ActionListener, Mediator {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(LoginFrame.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private ColleagueCheckbox checkGuest;
+	private ColleagueCheckbox checkLogin;
+	private ColleagueTextField textUser;
+	private ColleagueTextField textPass;
+	private ColleagueButton buttonOk;
+	private ColleagueButton buttonCancel;
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@SuppressWarnings("deprecation")
+	public LoginFrame(String title) {
+		super(title);
+		
+		if (flag) log.debug(">>>>> in class " + this.getClass().getSimpleName());
+		
+		setBackground(Color.LIGHT_GRAY);
+		
+		setLayout(new GridLayout(4, 2));
+		
+		createColleagues();
+		
+		add(checkGuest);
+		add(checkLogin);
+		add(new Label("Username:"));
+		add(textUser);
+		add(new Label("Password:"));
+		add(textPass);
+		add(buttonOk);
+		add(buttonCancel);
+		
+		colleagueChanged(checkGuest);
+		
+		pack();
+		show();
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	public void createColleagues() {
+		CheckboxGroup group = new CheckboxGroup();
+		checkGuest = new ColleagueCheckbox("Guest", group, true);
+		checkLogin = new ColleagueCheckbox("Login", group, false);
+		textUser = new ColleagueTextField("", 10);
+		textPass = new ColleagueTextField("", 10);
+		textPass.setEchoChar('*');
+		buttonOk = new ColleagueButton("OK");
+		buttonCancel = new ColleagueButton("Cancel");
+		
+		checkGuest.setMediator(this);
+		checkLogin.setMediator(this);
+		textUser.setMediator(this);
+		textPass.setMediator(this);
+		buttonOk.setMediator(this);
+		buttonCancel.setMediator(this);
+		
+		checkGuest.addItemListener(checkGuest);
+		checkLogin.addItemListener(checkLogin);
+		textUser.addTextListener(textUser);
+		textPass.addTextListener(textPass);
+		buttonOk.addActionListener(this);
+		buttonCancel.addActionListener(this);
+	}
+	
+	public void colleagueChanged(Colleague colleague) {
+		if (colleague == checkGuest || colleague == checkLogin) {
+			if (checkGuest.getState()) {
+				textUser.setColleagueEnabled(false);
+				textPass.setColleagueEnabled(false);
+				buttonOk.setColleagueEnabled(true);
+			} else {
+				textUser.setColleagueEnabled(true);
+				userpassChanged();
+			}
+		} else if (colleague == textUser || colleague == textPass) {
+			userpassChanged();
+		} else {
+			System.out.println("colleagueChaned : unknown colleague = " + colleague);
+		}
+	}
+	
+	private void userpassChanged() {
+		if (textUser.getText().length() > 0) {
+			textPass.setColleagueEnabled(true);
+			if (textPass.getText().length() > 0) {
+				buttonOk.setColleagueEnabled(true);
+			} else {
+				buttonOk.setColleagueEnabled(false);
+			}
+		} else {
+			textPass.setColleagueEnabled(false);
+			buttonOk.setColleagueEnabled(false);
+		}
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		System.out.println(e);
+		System.exit(0);
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+}
