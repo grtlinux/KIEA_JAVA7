b1c6866402b54805537652f8027996d0d1295975
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-05 23:57:50 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch05Singleton/v03/MainTest.java
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch05Singleton/v03/MainTest.java
@@ -61,6 +61,12 @@ public class MainTest {
 		if (flag) {
 			StatSingleton.getInstance();
 		}
+		
+		if (flag) {
+			for (int i=0; i < 20; i++) {
+				if (flag) System.out.format("%s\n", StatTriple.getInstance());
+			}
+		}
 	}
 	
 	public static void main(String[] args) throws Exception {
