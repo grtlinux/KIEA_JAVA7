f270aa4dafd8bc7d15e1d152af2242f3a1c48eb3
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-05 21:48:23 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch03Template/v02/AbstDisplay.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.designpattern.entrance.ch03Template.v02;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : AbstDisplay.java
+ *   -. Package    : tain.kr.com.test.designpattern.entrance.ch03Template.v02
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2017. 2. 5. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public abstract class AbstDisplay {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(AbstDisplay.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public AbstDisplay() {
+		
+		if (flag) log.debug(">>>>> in class " + this.getClass().getSimpleName());
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public abstract void open();
+	public abstract void print();
+	public abstract void close();
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public final void display() {
+		
+		open();
+		
+		for (int i=0; i < 5; i++) {
+			print();
+		}
+		
+		close();
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+}
---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch03Template/v02/DisplayString.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.designpattern.entrance.ch03Template.v02;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : DisplayString.java
+ *   -. Package    : tain.kr.com.test.designpattern.entrance.ch03Template.v02
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2017. 2. 5. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class DisplayString extends AbstDisplay {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(DisplayString.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private final String string;
+	private final int width;
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public DisplayString(String string) {
+		
+		if (flag) log.debug(">>>>> in class " + this.getClass().getSimpleName());
+		
+		this.string = string;
+		this.width = string.getBytes().length + 2;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Override
+	public void open() {
+		printLine();
+	}
+	
+	@Override
+	public void print() {
+		System.out.println("| " + this.string + " |");
+	}
+	
+	@Override
+	public void close() {
+		printLine();
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private void printLine() {
+		System.out.print("+");
+		
+		for (int i=0; i < width; i++) {
+			System.out.print("-");
+		}
+		
+		System.out.println("+");
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+}
