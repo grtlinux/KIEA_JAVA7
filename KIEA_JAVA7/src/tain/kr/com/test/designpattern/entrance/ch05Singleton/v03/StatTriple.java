06b2485f32ecaa85cf25f83be7aa82d46c1d0c1a
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-05 23:40:31 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/entrance/ch05Singleton/v03/StatTriple.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.designpattern.entrance.ch05Singleton.v03;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : StatTriple.java
+ *   -. Package    : tain.kr.com.test.designpattern.entrance.ch05Singleton.v03
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2017. 2. 5. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class StatTriple {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(StatTriple.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private final int id;
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private StatTriple(int id) {
+		
+		if (flag) log.debug(">>>>> in class " + this.getClass().getSimpleName());
+		
+		this.id = id;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public String toString() {
+		return String.format("[Triple id=%d]", this.id);
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+}
