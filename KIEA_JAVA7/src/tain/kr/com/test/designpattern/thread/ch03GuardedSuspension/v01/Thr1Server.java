b6c0c32c6af303f6adec9c8d8071b8a456b44c20
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-08 04:55:31 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/designpattern/thread/ch03GuardedSuspension/v01/Thr1Server.java
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/thread/ch03GuardedSuspension/v01/Thr1Server.java
@@ -19,6 +19,8 @@
  */
 package tain.kr.com.test.designpattern.thread.ch03GuardedSuspension.v01;
 
+import java.util.Random;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -35,24 +37,45 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
-public class Thr1Server {
+public final class Thr1Server extends Thread {
 
 	private static boolean flag = true;
 
 	private static final Logger log = Logger.getLogger(Thr1Server.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private Random random;
+	private FinlQueue queue;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
 	 * constructor
 	 */
-	public Thr1Server() {
+	public Thr1Server(FinlQueue queue, String nameThread, long seed) {
+		
+		super(nameThread);
+		this.queue = queue;
+		this.random = new Random(seed);
+		
 		if (flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Override
+	public void run() {
+		
+		for (int i=0; i < 1000; i++) {
+			AbstContent content = this.queue.get();
+			if (flag) System.out.printf("%s content %s\n", Thread.currentThread().getName(), content);
+			
+			try { Thread.sleep(random.nextInt(1000)); } catch (InterruptedException e) {}
+		}
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -69,9 +92,6 @@ public class Thr1Server {
 	 * static test method
 	 */
 	private static void test01(String[] args) throws Exception {
-
-		if (flag)
-			new Thr1Server();
 
 		if (flag) {
 
