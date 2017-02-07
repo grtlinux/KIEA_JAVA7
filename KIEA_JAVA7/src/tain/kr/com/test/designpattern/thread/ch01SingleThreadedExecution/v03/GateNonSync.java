2811bbf9daf93943041a345a6bc47995fefdf29b
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-07 19:29:45 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/thread/ch01SingleThreadedExecution/v03/GateNonSync.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.designpattern.thread.ch01SingleThreadedExecution.v03;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : GateNonSync.java
+ *   -. Package    : tain.kr.com.test.designpattern.thread.ch01SingleThreadedExecution.v03
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2017. 2. 7. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class GateNonSync extends AbstGate {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(GateNonSync.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private int counter = 0;
+	private String name = "Nobody";
+	private String address = "Nowhere";
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/*
+	 * constructor
+	 */
+	public GateNonSync(String gateName) {
+		
+		super(gateName);
+		
+		if (flag)
+			log.debug(">>>>> in class " + this.getClass().getSimpleName());
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public void pass(String name, String address) {
+		this.counter ++;
+		this.name = name;
+		this.address = address;
+		check();
+	}
+	
+	private void check() {
+		if (name.charAt(0) != address.charAt(0)) {
+			System.out.printf("***** BROKEN *****\n", this);
+		}
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	public String toString() {
+		return String.format("No.%d:%s, %s;", this.counter, this.name, this, address);
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/*
+	 * static test method
+	 */
+	private static void test01(String[] args) throws Exception {
+
+		if (flag)
+			new GateNonSync("Hello");
+
+		if (flag) {
+
+		}
+	}
+
+	/*
+	 * main method
+	 */
+	public static void main(String[] args) throws Exception {
+
+		if (flag)
+			log.debug(">>>>> " + new Object() {
+			}.getClass().getEnclosingClass().getName());
+
+		if (flag)
+			test01(args);
+	}
+}
