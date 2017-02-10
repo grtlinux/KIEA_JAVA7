385057466b016b614d0322f02361c27c2f6339f2
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-10 22:22:31 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/javaThreads/ch02/v01/example5/RandomCharacterGenerator.java
@@ -0,0 +1,148 @@
+/**
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.designpattern.javaThreads.ch02.v01.example5;
+
+import java.util.Random;
+
+import org.apache.log4j.Logger;
+
+import tain.kr.com.test.designpattern.javaThreads.ch02.v01.CharacterEventHandler;
+import tain.kr.com.test.designpattern.javaThreads.ch02.v01.CharacterListener;
+import tain.kr.com.test.designpattern.javaThreads.ch02.v01.CharacterSource;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : RandomCharacterGenerator.java
+ *   -. Package    : tain.kr.com.test.designpattern.javaThreads.ch02.v01.example5
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2017. 2. 10. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class RandomCharacterGenerator implements Runnable, CharacterSource {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger
+			.getLogger(RandomCharacterGenerator.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	static char[] chars;
+	static String charArray = "abcdefghijklmnopqrstuvwxyz0123456789";
+	static {
+		chars = charArray.toCharArray();
+	}
+
+	Random random;
+	CharacterEventHandler handler;
+
+	private volatile boolean done = false;
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/*
+	 * constructor
+	 */
+	public RandomCharacterGenerator() {
+		
+		random = new Random();
+		handler = new CharacterEventHandler();
+
+		if (flag)
+			log.debug(">>>>> in class " + this.getClass().getSimpleName());
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public int getPauseTime() {
+		return (int) (Math.max(1000, 5000 * random.nextDouble()));
+	}
+
+	public void addCharacterListener(CharacterListener cl) {
+		handler.addCharacterListener(cl);
+	}
+
+	public void removeCharacterListener(CharacterListener cl) {
+		handler.removeCharacterListener(cl);
+	}
+
+	public void nextCharacter() {
+		handler.fireNewCharacter(this, (int) chars[random.nextInt(chars.length)]);
+	}
+
+	public void run() {
+		while (!done) {
+			nextCharacter();
+			try {
+				Thread.sleep(getPauseTime());
+			} catch (InterruptedException ie) {
+				return;
+			}
+		}
+	}
+
+	public void setDone() {
+		done = true;
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/*
+	 * static test method
+	 */
+	private static void test01(String[] args) throws Exception {
+
+		if (flag)
+			new RandomCharacterGenerator();
+
+		if (flag) {
+
+		}
+	}
+
+	/*
+	 * main method
+	 */
+	public static void main(String[] args) throws Exception {
+
+		if (flag)
+			log.debug(">>>>> " + new Object() {
+			}.getClass().getEnclosingClass().getName());
+
+		if (flag)
+			test01(args);
+	}
+}
