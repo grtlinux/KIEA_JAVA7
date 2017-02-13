31e7acdcded80d65af7a0853b9c3148c89f247d5
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-14 08:09:47 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/javaThreads/ch03/v01/example9/RandomCharacterGenerator.java
@@ -0,0 +1,57 @@
+package tain.kr.com.test.designpattern.javaThreads.ch03.v01.example9;
+
+import java.util.Random;
+
+import tain.kr.com.test.designpattern.javaThreads.ch03.v01.CharacterEventHandler;
+import tain.kr.com.test.designpattern.javaThreads.ch03.v01.CharacterListener;
+import tain.kr.com.test.designpattern.javaThreads.ch03.v01.CharacterSource;
+
+public class RandomCharacterGenerator extends Thread implements CharacterSource {
+    static char[] chars;
+    static String charArray = "abcdefghijklmnopqrstuvwxyz0123456789";
+    static {
+        chars = charArray.toCharArray();
+    }
+
+    Random random;
+    CharacterEventHandler handler;
+
+    private volatile boolean done = false;
+
+    public RandomCharacterGenerator() {
+        random = new Random();
+        handler = new CharacterEventHandler();
+    }
+
+    public int getPauseTime() {
+        return (int) (Math.max(1000, 5000 * random.nextDouble()));
+    }
+
+    public void addCharacterListener(CharacterListener cl) {
+        handler.addCharacterListener(cl);
+    }
+
+    public void removeCharacterListener(CharacterListener cl) {
+        handler.removeCharacterListener(cl);
+    }
+
+    public void nextCharacter() {
+        handler.fireNewCharacter(this,
+                                (int) chars[random.nextInt(chars.length)]);
+    }
+
+    public void run() {
+        while (!done) {
+            nextCharacter();
+            try {
+                Thread.sleep(getPauseTime());
+            } catch (InterruptedException ie) {
+                return;
+            }
+        }
+    }
+
+    public void setDone() {
+        done = true;
+    }
+}
