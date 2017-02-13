ca2825e33d1b0d6d1a386331a91180315c4d6faa
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-14 08:04:37 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/javaThreads/ch03/v01/example7/AnimatedCharacterDisplayCanvas.java
@@ -0,0 +1,62 @@
+package tain.kr.com.test.designpattern.javaThreads.ch03.v01.example7;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import tain.kr.com.test.designpattern.javaThreads.ch03.v01.CharacterDisplayCanvas;
+import tain.kr.com.test.designpattern.javaThreads.ch03.v01.CharacterEvent;
+import tain.kr.com.test.designpattern.javaThreads.ch03.v01.CharacterListener;
+import tain.kr.com.test.designpattern.javaThreads.ch03.v01.CharacterSource;
+
+@SuppressWarnings("unused")
+public class AnimatedCharacterDisplayCanvas extends CharacterDisplayCanvas implements CharacterListener, Runnable {
+
+    /**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	private boolean done = false;
+    private int curX = 0;
+
+    public AnimatedCharacterDisplayCanvas() {
+    }
+
+    public AnimatedCharacterDisplayCanvas(CharacterSource cs) {
+        super(cs);
+    }
+
+    public synchronized void newCharacter(CharacterEvent ce) {
+        curX = 0;
+        tmpChar[0] = (char) ce.character;
+        repaint();
+    }
+
+	protected synchronized void paintComponent(Graphics gc) {
+        Dimension d = getSize();
+        gc.clearRect(0, 0, d.width, d.height);
+        if (tmpChar[0] == 0)
+            return;
+        int charWidth = fm.charWidth(tmpChar[0]);
+        gc.drawChars(tmpChar, 0, 1,
+                     curX++, fontHeight);
+    }
+
+    public void run() {
+        while (!getDone()) {
+            repaint();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ie) {
+                return;
+            }
+        }
+    }
+
+    public synchronized boolean getDone() {
+        return done;
+    }
+
+    public synchronized void setDone(boolean b) {
+        done = b;
+    }
+}
