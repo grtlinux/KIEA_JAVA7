eec6c2202f7bd32ac75c24120b9fd22cae7c142d
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-14 08:32:54 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/javaThreads/ch05/v01/example1/RandomCharacterGenerator.java
@@ -0,0 +1,78 @@
+package tain.kr.com.test.designpattern.javaThreads.ch05.v01.example1;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import tain.kr.com.test.designpattern.javaThreads.ch05.v01.CharacterEventHandler;
+import tain.kr.com.test.designpattern.javaThreads.ch05.v01.CharacterListener;
+import tain.kr.com.test.designpattern.javaThreads.ch05.v01.CharacterSource;
+
+public class RandomCharacterGenerator extends Thread implements CharacterSource {
+    private static char[] chars;
+    private static String charArray = "abcdefghijklmnopqrstuvwxyz0123456789";
+    static {
+        chars = charArray.toCharArray();
+    }
+
+    private Random random;
+    private CharacterEventHandler handler;
+    private boolean done = true;
+    private Lock lock = new ReentrantLock();
+    private Condition cv = lock.newCondition();
+
+    public RandomCharacterGenerator() {
+        random = new Random();
+        handler = new CharacterEventHandler();
+    }
+
+    public int getPauseTime() {
+        return (int) (Math.max(1000, 5000 * random.nextDouble()));
+    }
+
+    public void addCharacterListener(CharacterListener cl) {
+        handler.addCharacterListener(cl);
+    }
+
+    public void removeCharacterListener(CharacterListener cl) {
+        handler.removeCharacterListener(cl);
+    }
+
+    public void nextCharacter() {
+        handler.fireNewCharacter(this,
+                                (int) chars[random.nextInt(chars.length)]);
+    }
+
+    public void run() {
+        try {
+            lock.lock();
+            while (true) {
+                try {
+                    if (done) {
+                        cv.await();
+                    } else {
+                        nextCharacter();
+                        cv.await(getPauseTime(), TimeUnit.MILLISECONDS);
+                    }
+                } catch (InterruptedException ie) {
+                    return;
+                }
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void setDone(boolean b) {
+        try {
+            lock.lock();
+            done = b;
+
+            if (!done) cv.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
