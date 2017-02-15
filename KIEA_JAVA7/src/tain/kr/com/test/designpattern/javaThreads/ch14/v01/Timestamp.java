329636b724e27241c32962d24b9732d0bab6f668
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-15 19:43:18 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/javaThreads/ch14/v01/Timestamp.java
@@ -0,0 +1,48 @@
+package tain.kr.com.test.designpattern.javaThreads.ch14.v01;
+
+import java.util.concurrent.TimeUnit;
+
+public class Timestamp {
+
+    private long startTime;
+    private long stopTime;
+    private boolean stopped = false;
+    private TimeUnit ts;
+
+    public Timestamp() {
+	this(TimeUnit.NANOSECONDS);
+    }
+
+    public Timestamp(TimeUnit ts) {
+	this.ts = ts;
+        start();
+    }
+
+    public void start() {
+        startTime = System.nanoTime();
+	stopped = false;
+    }
+
+    public void stop() {
+        stopTime = System.nanoTime();
+	stopped = true;
+    }
+
+    public long elapsedTime() {
+	if (!stopped)
+	    throw new IllegalStateException("Timestamp not stopped");
+	return ts.convert(stopTime - startTime, TimeUnit.NANOSECONDS);
+    }
+
+    public String toString() {
+	try {
+	    return elapsedTime() + " " + ts;
+	} catch (IllegalStateException ise) {
+	    return "Timestamp (not stopped)";
+	}
+    }
+
+    public String units() {
+	return ts.toString();
+    }
+}
