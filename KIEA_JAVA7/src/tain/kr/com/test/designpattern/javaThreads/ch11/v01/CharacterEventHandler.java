90089f0560d6d6d0667badd8c8e81073c3472a1b
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-15 19:16:33 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/javaThreads/ch11/v01/CharacterEventHandler.java
@@ -0,0 +1,24 @@
+package tain.kr.com.test.designpattern.javaThreads.ch11.v01;
+
+import java.util.*;
+
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class CharacterEventHandler {
+    private Vector listeners = new Vector();
+
+    public void addCharacterListener(CharacterListener cl) {
+        listeners.add(cl);
+    }
+
+    public void removeCharacterListener(CharacterListener cl) {
+        listeners.remove(cl);
+    }
+
+    public void fireNewCharacter(CharacterSource source, int c) {
+        CharacterEvent ce = new CharacterEvent(source, c);
+        CharacterListener[] cl = (CharacterListener[] )
+                                 listeners.toArray(new CharacterListener[0]);
+        for (int i = 0; i < cl.length; i++)
+            cl[i].newCharacter(ce);
+    }
+}
---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/javaThreads/ch11/v01/CharacterListener.java
@@ -0,0 +1,5 @@
+package tain.kr.com.test.designpattern.javaThreads.ch11.v01;
+
+public interface CharacterListener {
+    public void newCharacter(CharacterEvent ce);
+}
