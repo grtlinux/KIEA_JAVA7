df17410d35fee4c6aedaef8d64a3c17231e3a6dc
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-16 07:20:28 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/spirit/v02/queue/ImplQueue.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.spirit.v02.queue;
+
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : ImplQueue.java
+ *   -. Package    : tain.kr.com.test.spirit.v02.queue
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2017. 2. 16. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public interface ImplQueue {
+	public abstract int put(Object object) throws Exception;
+	public abstract Object get() throws Exception;
+	public abstract Object get(long timeout) throws Exception;
+	public abstract void clear();
+	public abstract int getSize();
+}
