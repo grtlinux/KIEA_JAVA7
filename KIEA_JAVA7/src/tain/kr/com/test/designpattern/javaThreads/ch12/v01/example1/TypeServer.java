a61263627c88c879353b4087bdc9567a372bf5d6
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-15 19:32:21 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/javaThreads/ch12/v01/example1/TypeServer.java
@@ -0,0 +1,40 @@
+package tain.kr.com.test.designpattern.javaThreads.ch12.v01.example1;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import tain.kr.com.test.designpattern.javaThreads.ch12.v01.TCPServer;
+import tain.kr.com.test.designpattern.javaThreads.ch12.v01.TypeServerConstants;
+
+public class TypeServer extends TCPServer {
+    public void run(Socket data) {
+        try {
+            DataOutputStream dos =
+                   new DataOutputStream(data.getOutputStream());
+            dos.writeByte(TypeServerConstants.WELCOME);
+            DataInputStream dis =
+                  new DataInputStream(data.getInputStream());
+            while (true) {
+                byte b = dis.readByte();
+                if (b != TypeServerConstants.GET_STRING_REQUEST) {
+                    System.out.println("Client sent unknown request " + b);
+                    continue;
+                }
+                dos.writeByte(TypeServerConstants.GET_STRING_RESPONSE);
+                dos.writeUTF("Thisisateststring");
+                dos.flush();
+            }
+        } catch (Exception e) {
+            System.out.println("Client terminating: " + e);
+            return;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        TypeServer ts = new TypeServer();
+        ts.startServer(Integer.parseInt(args[0]));
+        System.out.println("Server ready and waiting...");
+    }
+}
