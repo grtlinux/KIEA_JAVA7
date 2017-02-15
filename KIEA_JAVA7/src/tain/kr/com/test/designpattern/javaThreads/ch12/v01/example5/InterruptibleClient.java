6c88b74089015cd3c4143754e6a36bc39f7bda36
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-15 19:38:39 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/designpattern/javaThreads/ch12/v01/example5/InterruptibleClient.java
@@ -0,0 +1,30 @@
+package tain.kr.com.test.designpattern.javaThreads.ch12.v01.example5;
+
+import java.io.InputStream;
+import java.net.Socket;
+
+import tain.kr.com.test.designpattern.javaThreads.ch12.v01.InterruptibleReader;
+
+public class InterruptibleClient extends InterruptibleReader {
+
+    public void processData(byte[] b, int n) {
+        System.out.println("Got data " + new String(b, 0, n));
+    }
+
+    public InterruptibleClient(InputStream is) {
+        super(is);
+    }
+
+    @SuppressWarnings("resource")
+	public static void main(String[] args) throws Exception {
+        Socket s = new Socket(args[0], Integer.parseInt(args[1]));
+        InputStream is = s.getInputStream();
+        InterruptibleClient c = new InterruptibleClient(is);
+        c.start();
+        System.out.println("Main thread sleeping");
+        Thread.sleep(10000);
+        System.out.println("Main thread woke up");
+        c.interrupt();
+        System.out.println("Main thread called interrupt");
+    }
+}
