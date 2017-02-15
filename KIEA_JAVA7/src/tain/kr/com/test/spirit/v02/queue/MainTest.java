07f2e2483b4f5fa0ce91d594346bf7681923edde
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-16 07:26:10 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/spirit/v02/queue/MainTest.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.spirit.v02.queue;
+
+import org.apache.log4j.Logger;
+
+import tain.kr.com.test.queue.v02.QueueVector;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : MainTest.java
+ *   -. Package    : tain.kr.com.test.spirit.v02.queue
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2017. 2. 16. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class MainTest {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(MainTest.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/*
+	 * constructor
+	 */
+	public MainTest() {
+		if (flag)
+			log.debug(">>>>> in class " + this.getClass().getSimpleName());
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static QueueMsg queue = null;
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/*
+	 * static test method
+	 */
+	private static void test01(String[] args) throws Exception {
+
+		if (flag)
+			new MainTest();
+
+		if (flag) {
+			/*
+			 * queue
+			 */
+			queue = new QueueMsg();
+		}
+		
+		if (flag) {
+			/*
+			 * get thread
+			 */
+			new Thread() {
+				@Override
+				public void run() {
+					try {
+						String msg = (String) queue.get();
+						if (flag) System.out.printf("GET[%s]\n", msg);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}.start();
+		}
+		
+		try { Thread.sleep(2000); } catch (InterruptedException e) {}
+		
+		if (flag) {
+			/*
+			 * put thread
+			 */
+			new Thread() {
+				@Override
+				public void run() {
+					try {
+						String msg = "Hello, world!!!!";
+						int size = queue.put(msg);
+						if (flag) System.out.printf("PUT[%d:%s]\n", size, msg);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}.start();
+		}
+	}
+
+	/*
+	 * main method
+	 */
+	public static void main(String[] args) throws Exception {
+
+		if (flag)
+			log.debug(">>>>> " + new Object() {
+			}.getClass().getEnclosingClass().getName());
+
+		if (flag)
+			test01(args);
+	}
+}
