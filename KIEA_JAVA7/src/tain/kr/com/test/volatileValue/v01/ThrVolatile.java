f23838aaa1ee72a2ed57c9c977452883132ce8d0
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-18 09:50:32 +0900

    .

---
+++ KIEA_JAVA7/src/tain/kr/com/test/volatileValue/v01/ThrVolatile.java
@@ -0,0 +1,128 @@
+/**
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.volatileValue.v01;
+
+import java.util.Date;
+import java.util.Random;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : ThrVolatile.java
+ *   -. Package    : tain.kr.com.test.volatileValue.v01
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2017. 2. 18. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public final class ThrVolatile extends Thread {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(ThrVolatile.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private final Random random;
+	
+	private volatile boolean stop = false;
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/*
+	 * constructor
+	 */
+	public ThrVolatile(String thrName) {
+		
+		super(thrName);
+		
+		this.random = new Random(new Date().getTime());
+		
+		if (flag)
+			log.debug(">>>>> in class " + this.getClass().getSimpleName());
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Override
+	public void run() {
+		
+		for (int i=0; !this.stop; i++) {
+			
+			if (flag) System.out.printf("%s - %d\n", this, i);
+			
+			int timeSleep = 1000 + 100 * this.random.nextInt(100);
+			try { Thread.sleep(timeSleep); } catch (InterruptedException e) {}
+		}
+		
+		if (flag) System.out.println(this);
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public void requestStop() {
+		this.stop = true;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public String toString() {
+		return String.format("[%s]", Thread.currentThread().getName());
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/*
+	 * static test method
+	 */
+	private static void test01(String[] args) throws Exception {
+
+		if (flag) {
+
+		}
+	}
+
+	/*
+	 * main method
+	 */
+	public static void main(String[] args) throws Exception {
+
+		if (flag)
+			log.debug(">>>>> " + new Object() {
+			}.getClass().getEnclosingClass().getName());
+
+		if (flag)
+			test01(args);
+	}
+}
