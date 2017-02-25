7bb5457c9bf5ea8ddc5d5cbc4ff96fb5c3f10390
Kiea Seok Kang <grtlinux@gmail.com>
2017-02-26 02:06:39 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/network/v01/ThrClient.java
+++ KIEA_JAVA7/src/tain/kr/com/test/network/v01/ThrClient.java
@@ -255,7 +255,7 @@ public final class ThrClient extends Thread {
 									 * EOF(End Of File) : end of stream -> end of process 
 									 */
 									if (flag) System.out.printf("%s [STATUS] read data of EOF...\n", Thread.currentThread().getName());
-									if (flag) printInfo();
+									if (!flag) printInfo();
 									throw new Exception("EOF: end of process because return value of read is -1.");
 								}
 							} catch (SocketTimeoutException e) {
@@ -270,7 +270,7 @@ public final class ThrClient extends Thread {
 								 * SocketException -> end of process
 								 */
 								if (flag) System.out.printf("%s [STATUS] SocketException...\n", Thread.currentThread().getName());
-								if (flag) e.printStackTrace();
+								if (!flag) printInfo();
 								throw e;
 							} catch (Exception e) {
 								/*
