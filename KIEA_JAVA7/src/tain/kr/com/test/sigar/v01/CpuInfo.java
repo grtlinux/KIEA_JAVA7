51e0c732aefc16fd2d916bf741ec667b6b632134
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-17 22:51:10 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/CpuInfo.java
+++ KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/CpuInfo.java
@@ -21,7 +21,10 @@ package tain.kr.com.test.sigar.v01;
 
 import org.apache.log4j.Logger;
 import org.hyperic.sigar.CpuPerc;
+import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
+import org.hyperic.sigar.SigarLoader;
+import org.hyperic.sigar.cmd.Shell;
 import org.hyperic.sigar.cmd.SigarCommandBase;
 
 /**
@@ -45,16 +48,26 @@ public final class CpuInfo extends SigarCommandBase {
 	private static final Logger log = Logger.getLogger(CpuInfo.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private boolean displayTimes = true;
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
 	 * constructor
 	 */
-	public CpuInfo() {
+	public CpuInfo(Shell shell) {
+		
+		super(shell);
+		
 		if (flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
 	}
 
+	public CpuInfo() {
+		super();
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/* (non-Javadoc)
@@ -62,12 +75,60 @@ public final class CpuInfo extends SigarCommandBase {
 	 */
 	@Override
 	public void output(String[] arg0) throws SigarException {
-		// TODO Auto-generated method stub
-		
+        org.hyperic.sigar.CpuInfo[] infos =
+                this.sigar.getCpuInfoList();
+
+            CpuPerc[] cpus =
+                this.sigar.getCpuPercList();
+
+            org.hyperic.sigar.CpuInfo info = infos[0];
+            long cacheSize = info.getCacheSize();
+            println("Vendor........." + info.getVendor());
+            println("Model.........." + info.getModel());
+            println("Mhz............" + info.getMhz());
+            println("Total CPUs....." + info.getTotalCores());
+            if ((info.getTotalCores() != info.getTotalSockets()) ||
+                (info.getCoresPerSocket() > info.getTotalCores()))
+            {
+                println("Physical CPUs.." + info.getTotalSockets());
+                println("Cores per CPU.." + info.getCoresPerSocket());
+            }
+
+            if (cacheSize != Sigar.FIELD_NOTIMPL) {
+                println("Cache size...." + cacheSize);
+            }
+            println("");
+
+            if (!this.displayTimes) {
+                return;
+            }
+
+            for (int i=0; i<cpus.length; i++) {
+                println("CPU " + i + ".........");
+                output(cpus[i]);
+            }
+
+            println("Totals........");
+            output(this.sigar.getCpuPerc());
 	}
 
 	private void output(CpuPerc cpu) {
-		
+        println("User Time....." + CpuPerc.format(cpu.getUser()));
+        println("Sys Time......" + CpuPerc.format(cpu.getSys()));
+        println("Idle Time....." + CpuPerc.format(cpu.getIdle()));
+        println("Wait Time....." + CpuPerc.format(cpu.getWait()));
+        println("Nice Time....." + CpuPerc.format(cpu.getNice()));
+        println("Combined......" + CpuPerc.format(cpu.getCombined()));
+        println("Irq Time......" + CpuPerc.format(cpu.getIrq()));
+        if (SigarLoader.IS_LINUX) {
+            println("SoftIrq Time.." + CpuPerc.format(cpu.getSoftIrq()));
+            println("Stolen Time...." + CpuPerc.format(cpu.getStolen()));
+        }
+        println("");
+	}
+	
+	public String getUsageShort() {
+		return "Display cpu information";
 	}
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -91,7 +152,11 @@ public final class CpuInfo extends SigarCommandBase {
 			new CpuInfo();
 
 		if (flag) {
-
+			/*
+			 * begin
+			 */
+			new CpuInfo().processCommand(args);
+			
 		}
 	}
 
