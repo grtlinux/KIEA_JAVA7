dfeea33bbd5d00673316ae6617f48c80bab5eb8e
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-18 13:58:58 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/FileVersionInfo.java
+++ KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/FileVersionInfo.java
@@ -19,8 +19,14 @@
  */
 package tain.kr.com.test.sigar.v01;
 
+import java.io.File;
+import java.util.Iterator;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.hyperic.sigar.SigarException;
+import org.hyperic.sigar.win32.FileVersion;
+import org.hyperic.sigar.win32.Win32;
 
 /**
  * Code Templates > Comments > Types
@@ -52,7 +58,7 @@ public final class FileVersionInfo extends SigarCommandBase {
 		
 		super(shell);
 		
-		if (flag)
+		if (!flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
 	}
 
@@ -61,6 +67,17 @@ public final class FileVersionInfo extends SigarCommandBase {
 	}
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Override
+	protected boolean validateArgs(String[] args) {
+		return args.length >= 1;
+	}
+	
+	@Override
+	public String getUsageShort() {
+		return "Display file version info";
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/* (non-Javadoc)
@@ -68,10 +85,57 @@ public final class FileVersionInfo extends SigarCommandBase {
 	 */
 	@Override
 	public void output(String[] args) throws SigarException {
-		// TODO Auto-generated method stub
 		
+		for (int i=0; i < args.length; i++) {
+			String exe = args[i];
+			if (new File(exe).exists()) {
+				output(exe);
+			} else {
+				long[] pids = this.shell.findPids(exe);
+				for (int j=0; j < pids.length; j++) {
+					try {
+						output(sigar.getProcExe(pids[j]).getName());
+					} catch (SigarException e) {
+						println(exe + ": " + e.getMessage());
+					}
+				}
+			}
+		}
 	}
 
+	private void output(String key, String val) {
+		
+		final int max = 20;
+		int len = max - key.length();
+		
+		StringBuffer sb = new StringBuffer();
+		sb.append("  ").append(key);
+		while (len-- > 0) {
+			sb.append('.');
+		}
+		sb.append(val);
+		
+		println(sb.toString());
+	}
+	
+	public void output(String exe) throws SigarException {
+		
+		FileVersion info = Win32.getFileVersion(exe);
+		if (info == null) {
+			return;
+		}
+		
+		println("Version info for file '" + exe + "':");
+		output("FileVersion", info.getFileVersion());
+		output("ProductVersion", info.getProductVersion());
+		
+		for (@SuppressWarnings("unchecked")
+		Iterator<Map.Entry<String, String>> it = info.getInfo().entrySet().iterator(); it.hasNext();) {
+			Map.Entry<String, String> entry = (Map.Entry<String, String>) it.next();
+			output((String) entry.getKey(), (String) entry.getValue());
+		}
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -92,7 +156,11 @@ public final class FileVersionInfo extends SigarCommandBase {
 			new FileVersionInfo();
 
 		if (flag) {
-
+			/*
+			 * begin
+			 */
+			new FileVersionInfo().processCommand(new String[] { "N:/PROG/jdk1.7.0_79/bin/javaw.exe" });
+			//new FileVersionInfo().processCommand(args);
 		}
 	}
 
