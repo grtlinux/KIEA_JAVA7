28164b2dd9019864141db58c58a1b2256323c21e
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-18 20:15:04 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/Ps.java
+++ KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/Ps.java
@@ -19,8 +19,20 @@
  */
 package tain.kr.com.test.sigar.v01;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.hyperic.sigar.ProcCredName;
+import org.hyperic.sigar.ProcMem;
+import org.hyperic.sigar.ProcState;
+import org.hyperic.sigar.ProcTime;
+import org.hyperic.sigar.ProcUtil;
+import org.hyperic.sigar.Sigar;
 import org.hyperic.sigar.SigarException;
+import org.hyperic.sigar.SigarProxy;
 
 /**
  * Code Templates > Comments > Types
@@ -81,10 +93,127 @@ public final class Ps extends SigarCommandBase {
 	@Override
 	public void output(String[] args) throws SigarException {
 		
+		long[] pids;
+		if (args.length == 0) {
+			pids = this.proxy.getProcList();
+		} else {
+			pids = this.shell.findPids(args);
+		}
+
+		for (int i=0; i < pids.length; i++) {
+			long pid = pids[i];
+			try {
+				output(pid);
+			} catch (SigarException e) {
+				this.err.println("Exception getting process info for " + pid + ": " + e.getMessage());
+			}
+		}
+	}
+	
+	public void output(long pid) throws SigarException {
+		
+		println(join(getInfo(this.proxy, pid)));
 	}
 	
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	public static String join(List<String> infos) {
+		
+		StringBuffer sb = new StringBuffer();
+		
+		int i = 0;
+		for (String info : infos) {
+			if (i != 0)
+				sb.append("\t");
+			sb.append(info);
+			i++;
+		}
+		
+		return sb.toString();
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@SuppressWarnings("deprecation")
+	public static List<String> getInfo(SigarProxy sigar, long pid) throws SigarException {
+		
+		ProcState state = sigar.getProcState(pid);
+		ProcTime time = null;
+		String unknown = "???";
+
+		List<String> info = new ArrayList<String>();
+		info.add(String.valueOf(pid));
+
+		try {
+			ProcCredName cred = sigar.getProcCredName(pid);
+			info.add(cred.getUser());
+		} catch (SigarException e) {
+			info.add(unknown);
+		}
+
+		try {
+			time = sigar.getProcTime(pid);
+			info.add(getStartTime(time.getStartTime()));
+		} catch (SigarException e) {
+			info.add(unknown);
+		}
+
+		try {
+			ProcMem mem = sigar.getProcMem(pid);
+			info.add(Sigar.formatSize(mem.getSize()));
+			info.add(Sigar.formatSize(mem.getRss()));
+			info.add(Sigar.formatSize(mem.getShare()));
+		} catch (SigarException e) {
+			info.add(unknown);
+		}
+
+		info.add(String.valueOf(state.getState()));
+
+		if (time != null) {
+			info.add(getCpuTime(time));
+		} else {
+			info.add(unknown);
+		}
+
+		String name = ProcUtil.getDescription(sigar, pid);
+		info.add(name);
+
+		return info;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public static String getCpuTime(long total) {
+		
+		long t = total / 1000;
+		return String.format("%02d:%02d", t / 60, t % 60);
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	public static String getCpuTime(ProcTime time) {
+		
+		return getCpuTime(time.getTotal());
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private static String getStartTime(long time) {
+		
+		if (time == 0) {
+			return "00:00";
+		}
+		
+		long timeNow = System.currentTimeMillis();
+		String fmt = "MMMd";
+		
+		if ((timeNow - time) < ((60 * 60 * 24) * 1000)) {
+			fmt = "HH:mm";
+		}
+		
+		return new SimpleDateFormat(fmt).format(new Date(time));
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
