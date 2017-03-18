faff30dee7624bae4b191df6dc5e94007a823982
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-18 22:22:01 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/ProcFileInfo.java
+++ KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/ProcFileInfo.java
@@ -20,6 +20,11 @@
 package tain.kr.com.test.sigar.v01;
 
 import org.apache.log4j.Logger;
+import org.hyperic.sigar.ProcExe;
+import org.hyperic.sigar.ProcFd;
+import org.hyperic.sigar.SigarException;
+import org.hyperic.sigar.SigarNotImplementedException;
+import org.hyperic.sigar.SigarPermissionDeniedException;
 
 /**
  * Code Templates > Comments > Types
@@ -35,7 +40,7 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
-public class ProcFileInfo {
+public final class ProcFileInfo extends SigarCommandBase {
 
 	private static boolean flag = true;
 
@@ -47,13 +52,70 @@ public class ProcFileInfo {
 	/*
 	 * constructor
 	 */
-	public ProcFileInfo() {
-		if (flag)
+	public ProcFileInfo(Shell shell) {
+		super(shell);
+		if (!flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
+	}
+	
+	public ProcFileInfo() {
+		super();
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	protected boolean validateArgs(String[] args) {
+		return true;
+	}
+
+	public String getUsageShort() {
+		return "Display process file info";
+	}
+
+	public boolean isPidCompleter() {
+		return true;
+	}
+
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	public void output(String[] args) throws SigarException {
+		
+		long[] pids = this.shell.findPids(args);
+
+		for (int i=0; i<pids.length; i++) {
+			try {
+				output(pids[i]);
+			} catch (SigarPermissionDeniedException e) {
+				println(this.shell.getUserDeniedMessage(pids[i]));
+			} catch (SigarException e) {
+				println("(" + e.getMessage() + ")");
+			}
+			
+			println("\n------------------------\n");
+		}
+	}
+
+	public void output(long pid) throws SigarException {
+		
+		println("pid=" + pid);
+
+		try {
+			ProcFd fd = sigar.getProcFd(pid);
+			println("open file descriptors=" + fd.getTotal());
+		} catch (SigarNotImplementedException e) {}
+
+		ProcExe exe = sigar.getProcExe(pid);
+		String name = exe.getName();
+		
+		if (name.length() == 0) {
+			name = "unknown";
+		}
+		
+		println("name=" + name);
+
+		println("cwd=" + exe.getCwd());
+	}
+
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -74,7 +136,10 @@ public class ProcFileInfo {
 			new ProcFileInfo();
 
 		if (flag) {
-
+			/*
+			 * begin
+			 */
+			new ProcFileInfo().processCommand(args);
 		}
 	}
 
