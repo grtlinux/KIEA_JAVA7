2c723daf5ccdf3cd3d58e9e31380f8b819eb3443
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-18 23:37:47 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/Ulimit.java
+++ KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/Ulimit.java
@@ -20,6 +20,9 @@
 package tain.kr.com.test.sigar.v01;
 
 import org.apache.log4j.Logger;
+import org.hyperic.sigar.ResourceLimit;
+import org.hyperic.sigar.SigarException;
+import org.hyperic.sigar.jmx.SigarInvokerJMX;
 
 /**
  * Code Templates > Comments > Types
@@ -42,6 +45,10 @@ public final class Ulimit extends SigarCommandBase {
 	private static final Logger log = Logger.getLogger(Ulimit.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private SigarInvokerJMX invoker;
+	private String mode;
+
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
@@ -58,6 +65,15 @@ public final class Ulimit extends SigarCommandBase {
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	public String getUsageShort() {
+		return "Display system resource limits";
+	}
+
+	protected boolean validateArgs(String[] args) {
+		return true;
+	}
+
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -68,6 +84,52 @@ public final class Ulimit extends SigarCommandBase {
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static String format(long val) {
+		
+		if (val == ResourceLimit.INFINITY()) {
+			return "unlimited";
+		} else {
+			return String.valueOf(val);
+		}
+	}
+
+	private String getValue(String attr) throws SigarException {
+		
+		Long val = (Long)this.invoker.invoke(attr + this.mode);
+		return format(val.longValue());
+	}
+
+	public void output(String[] args) throws SigarException {
+
+		this.mode = "Cur";
+		this.invoker = SigarInvokerJMX.getInstance(this.proxy, "Type=ResourceLimit");
+
+		for (int i=0; i<args.length; i++) {
+			String arg = args[i];
+			if (arg.equals("-H")) {
+				this.mode = "Max";
+			} else if (arg.equals("-S")) {
+				this.mode = "Cur";
+			} else {
+				throw new SigarException("Unknown argument: " + arg);
+			}
+		}
+
+		println("core file size......." + getValue("Core"));
+		println("data seg size........" + getValue("Data"));
+		println("file size............" + getValue("FileSize"));
+		println("pipe size............" + getValue("PipeSize"));
+		println("max memory size......" + getValue("Memory"));
+		println("open files..........." + getValue("OpenFiles"));
+		println("stack size..........." + getValue("Stack"));
+		println("cpu time............." + getValue("Cpu"));
+		println("max user processes..." + getValue("Processes"));
+		println("virtual memory......." + getValue("VirtualMemory"));
+	}
+
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
