e3caea8ba2db73ae5391e05ea94102ba6aca90d5
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-18 14:24:32 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/Ifconfig.java
+++ KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/Ifconfig.java
@@ -19,7 +19,15 @@
  */
 package tain.kr.com.test.sigar.v01;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.apache.log4j.Logger;
+import org.hyperic.sigar.NetFlags;
+import org.hyperic.sigar.NetInterfaceConfig;
+import org.hyperic.sigar.NetInterfaceStat;
+import org.hyperic.sigar.Sigar;
+import org.hyperic.sigar.SigarException;
 
 /**
  * Code Templates > Comments > Types
@@ -35,7 +43,7 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
-public class Ifconfig {
+public final class Ifconfig extends SigarCommandBase {
 
 	private static boolean flag = true;
 
@@ -47,13 +55,134 @@ public class Ifconfig {
 	/*
 	 * constructor
 	 */
-	public Ifconfig() {
-		if (flag)
+	public Ifconfig(Shell shell) {
+		super(shell);
+		if (!flag)
 			log.debug(">>>>> in class " + this.getClass().getSimpleName());
+	}
+	
+	public Ifconfig() {
+		super();
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Override
+	protected boolean validateArgs(String[] args) {
+		return args.length <= 1;
+	}
+	
+	public String getSyntaxArgs() {
+		return "[interface]";
+	}
+	
+	public String getUsageShort() {
+		return "Network interface information";
+	}
+	
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public Collection getCompletions() {
+		String[] ifNames;
+		
+		try {
+			ifNames = this.proxy.getNetInterfaceList();
+		} catch (SigarException e) {
+			return super.getCompletions();
+		}
+		
+		return Arrays.asList(ifNames);
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	/* (non-Javadoc)
+	 * @see tain.kr.com.test.sigar.v01.SigarCommandBase#output(java.lang.String[])
+	 */
+	@Override
+	public void output(String[] args) throws SigarException {
+		
+		String[] ifNames;
+		
+		if (args.length == 1) {
+			ifNames = args;
+		} else {
+			ifNames = this.proxy.getNetInterfaceList();
+		}
+		
+		for (int i=0; i < ifNames.length; i++) {
+			try {
+				output(ifNames[i]);
+			} catch (SigarException e) {
+				println(ifNames[i] + "\t" + e.getMessage());
+			}
+		}
+	}
+	
+	public void output(String name) throws SigarException {
+		
+		NetInterfaceConfig ifconfig = this.sigar.getNetInterfaceConfig(name);
+		long flags = ifconfig.getFlags();
+		
+		String hwaddr = "";
+		if (!NetFlags.NULL_HWADDR.equals(ifconfig.getHwaddr())) {
+			hwaddr = " HWaddr " + ifconfig.getHwaddr();
+		}
+		
+		if (!ifconfig.getName().equals(ifconfig.getDescription())) {
+			println(ifconfig.getDescription());
+		}
+		
+		println(ifconfig.getName() + "\t" + "Link encap:" + ifconfig.getType() + hwaddr);
+		
+		String ptp = "";
+		if ((flags & NetFlags.IFF_POINTOPOINT) > 0) {
+			ptp = "  P-t-P:" + ifconfig.getDescription();
+		}
+		
+		String bcast = "";
+		if ((flags & NetFlags.IFF_BROADCAST) > 0) {
+			bcast = "  Bcast:" + ifconfig.getBroadcast();
+		}
+		
+		println("\t" + "inet addr:" + ifconfig.getAddress() + ptp + bcast + "  Mask:" + ifconfig.getNetmask());
+		
+		println("\t" + NetFlags.getIfFlagsString(flags) + " MTU:" + ifconfig.getMtu() + "  Metric:" + ifconfig.getMetric());
+		
+		try {
+			NetInterfaceStat ifstat = this.sigar.getNetInterfaceStat(name);
+			
+			println("\t" +
+					"RX packets:" + ifstat.getRxPackets() +
+					" errors:" + ifstat.getRxErrors() +
+					" dropped:" + ifstat.getRxDropped() +
+					" overruns:" + ifstat.getRxOverruns() +
+					" frame:" + ifstat.getRxFrame());
+
+			println("\t" +
+					"TX packets:" + ifstat.getTxPackets() +
+					" errors:" + ifstat.getTxErrors() +
+					" dropped:" + ifstat.getTxDropped() +
+					" overruns:" + ifstat.getTxOverruns() +
+					" carrier:" + ifstat.getTxCarrier());
+			
+			println("\t" + "collisions:" + ifstat.getTxCollisions());
+			
+			long rxBytes = ifstat.getRxBytes();
+			long txBytes = ifstat.getTxBytes();
+			
+			println("\t" +
+					"RX bytes:" + rxBytes +
+					" (" + Sigar.formatSize(rxBytes) + ")" +
+					"  " +
+					"TX bytes:" + txBytes +
+					" (" + Sigar.formatSize(txBytes) + ")");
+			
+		} catch (SigarException e) {
+		}
+		
+		println("");
+	}
+	
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -70,11 +199,11 @@ public class Ifconfig {
 	 */
 	private static void test01(String[] args) throws Exception {
 
-		if (flag)
-			new Ifconfig();
-
 		if (flag) {
-
+			/*
+			 * begin
+			 */
+			new Ifconfig().processCommand(args);
 		}
 	}
 
