5b01761a4dccad61cdc81fa9b1f14ace11407804
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-18 22:52:10 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/Top.java
+++ KIEA_JAVA7/src/tain/kr/com/test/sigar/v01/Top.java
@@ -19,7 +19,16 @@
  */
 package tain.kr.com.test.sigar.v01;
 
+import java.util.List;
+
 import org.apache.log4j.Logger;
+import org.hyperic.sigar.CpuPerc;
+import org.hyperic.sigar.ProcCpu;
+import org.hyperic.sigar.ProcStat;
+import org.hyperic.sigar.Sigar;
+import org.hyperic.sigar.SigarException;
+import org.hyperic.sigar.SigarProxy;
+import org.hyperic.sigar.SigarProxyCache;
 
 /**
  * Code Templates > Comments > Types
@@ -35,13 +44,19 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
-public class Top {
+public final class Top {
 
 	private static boolean flag = true;
 
 	private static final Logger log = Logger.getLogger(Top.class);
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static final int SLEEP_TIME = 1000 * 5;
+
+	private static final String HEADER =
+		"PID\tUSER\tSTIME\tSIZE\tRSS\tSHARE\tSTATE\tTIME\t%CPU\tCOMMAND";
+
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
@@ -53,6 +68,16 @@ public class Top {
 	}
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static String toString(ProcStat stat) {
+		return
+			stat.getTotal()    + " processes: " +
+			stat.getSleeping() + " sleeping, " +
+			stat.getRunning()  + " running, " +
+			stat.getZombie()   + " zombie, " +
+			stat.getStopped()  + " stopped... " + stat.getThreads() + " threads";
+	}
+
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -70,11 +95,59 @@ public class Top {
 	 */
 	private static void test01(String[] args) throws Exception {
 
-		if (flag)
-			new Top();
-
 		if (flag) {
+			/*
+			 * begin
+			 */
+			Sigar sigarImpl = new Sigar();
 
+			SigarProxy sigar =
+				SigarProxyCache.newInstance(sigarImpl, SLEEP_TIME);
+
+			while (true) {
+				Shell.clearScreen();
+
+				System.out.println(Uptime.getInfo(sigar));
+
+				System.out.println(toString(sigar.getProcStat()));
+
+				System.out.println(sigar.getCpuPerc());
+
+				System.out.println(sigar.getMem());
+
+				System.out.println(sigar.getSwap());
+
+				System.out.println();
+
+				System.out.println(HEADER);
+
+				long[] pids = Shell.getPids(sigar, args);
+
+				for (int i=0; i<pids.length; i++) {
+					long pid = pids[i];
+
+					String cpuPerc = "?";
+
+					List info;
+					try {
+						info = Ps.getInfo(sigar, pid);
+					} catch (SigarException e) {
+						continue; //process may have gone away
+					}
+					try {
+						ProcCpu cpu = sigar.getProcCpu(pid);
+						cpuPerc = CpuPerc.format(cpu.getPercent());
+					} catch (SigarException e) {
+					}
+
+					info.add(info.size()-1, cpuPerc);
+
+					System.out.println(Ps.join(info));
+				}
+
+				Thread.sleep(SLEEP_TIME);
+				SigarProxyCache.clear(sigar);
+			}
 		}
 	}
 
