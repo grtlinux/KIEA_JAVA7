f05ee9a8ce862998314745983d99ac5a35335a62
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-19 04:04:41 +0900

    .

---
+++ KIEA_JAVA7/projects/bin/startBeanClient.bat
@@ -0,0 +1,34 @@
+@echo off
+@setlocal
+
+::----------------------------------------------
+:label_env1
+set CURRENT_DIR=%CD%
+set ENV_CMD=%CURRENT_DIR%\_env.cmd
+
+call %ENV_CMD% _env.cmd
+if errorlevel 1 goto label_end
+
+::----------------------------------------------
+:label_start
+
+set JVM_OPTIONS=
+set JVM_OPTIONS=%JVM_OPTIONS% -Dtain.co.kr=TAIN
+set JVM_OPTIONS=%JVM_OPTIONS% -DrsrcMainClass=tain.kr.com.proj.cosmarter.v04.main.client.BeanClient
+set JVM_OPTIONS=%JVM_OPTIONS% -Dtain.cosmarter.v04.client.log.flag=false
+set JVM_OPTIONS=%JVM_OPTIONS% -Dtain.cosmarter.v04.client.charset=euc-kr
+set JVM_OPTIONS=%JVM_OPTIONS% -Dfile.encoding=euc-kr
+
+set JAR_FILE=%CURRENT_DIR%\tain-cosmarter-1.04.jar
+
+java %JVM_OPTIONS% -jar %JAR_FILE%
+
+::----------------------------------------------
+:label_end
+echo The end of the command
+
+@endlocal
+
+
+
+
