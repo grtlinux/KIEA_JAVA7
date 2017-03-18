069d9ed6ba138ae11325feac6ad281ec510f1a30
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-19 05:07:58 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/commons/net/v01/ftp/parser/ParserInitializationException.java
+++ KIEA_JAVA7/src/tain/kr/com/commons/net/v01/ftp/parser/ParserInitializationException.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.commons.net.ftp.parser;
+package tain.kr.com.commons.net.v01.ftp.parser;
 
 /**
  * This class encapsulates all errors that may be thrown by
