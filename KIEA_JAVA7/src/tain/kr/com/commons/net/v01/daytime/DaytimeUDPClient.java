4acd03dc53f4ad660dbd617d4cae16cbf648b1b2
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-19 05:06:36 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/commons/net/v01/daytime/DaytimeUDPClient.java
+++ KIEA_JAVA7/src/tain/kr/com/commons/net/v01/daytime/DaytimeUDPClient.java
@@ -15,13 +15,13 @@
  * limitations under the License.
  */
 
-package org.apache.commons.net.daytime;
+package tain.kr.com.commons.net.v01.daytime;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 
-import org.apache.commons.net.DatagramSocketClient;
+import tain.kr.com.commons.net.v01.DatagramSocketClient;
 
 /**
  * The DaytimeUDPClient class is a UDP implementation of a client for the
