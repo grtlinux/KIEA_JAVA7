9fb88db459bad4fff68e51a45c4e133af9a3cb36
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-19 15:34:42 +0900

    .

---
+++ KIEA_JAVA7/src/test/v01/CalculatorTestSuite.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016, 2017 TAIN, Inc.
+ *
+ */
+package test.v01;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : CalculatorTestSuit.java
+ *   -. Package    : test.v01
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2017. 3. 19. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+@RunWith(value = Suite.class)
+@SuiteClasses(value = { CalculatorTest.class, ParameterizedTest.class })
+public class CalculatorTestSuite {
+
+}
