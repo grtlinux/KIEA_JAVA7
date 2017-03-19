7b63e5a3a7ed75229ebcb0d637b91c96483de935
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-19 14:56:27 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/hello/v01/HelloTestMain.java
+++ KIEA_JAVA7/src/tain/kr/com/test/hello/v01/HelloTestMain.java
@@ -2,28 +2,32 @@ package tain.kr.com.test.hello.v01;
 
 import org.apache.log4j.Logger;
 
+import tain.kr.com.test.version.v01.Version;
+
 public class HelloTestMain {
 
-	final static Logger logger = Logger.getLogger(HelloTestMain.class);
-	
 	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(Version.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
 	
 	public static void main(String[] args) throws Exception {
 		
 		if (flag) {
-			if (logger.isDebugEnabled()) {
-				logger.debug("ENABLE debug : " + "Hello, world!!! æ»≥Á«œººø‰.");
+			if (log.isDebugEnabled()) {
+				log.debug("ENABLE debug : " + "Hello, world!!! æ»≥Á«œººø‰.");
 			}
 		}
 		
 		if (flag) {
 			System.out.println("Hello, world!!! æ»≥Á«œººø‰.");
 			
-			logger.debug("debug : " + "Hello, world!!! æ»≥Á«œººø‰.");
-			logger.info ("info  : " + "Hello, world!!! æ»≥Á«œººø‰.");
-			logger.warn ("warn  : " + "Hello, world!!! æ»≥Á«œººø‰.");
-			logger.error("error : " + "Hello, world!!! æ»≥Á«œººø‰.");
-			logger.fatal("fatal : " + "Hello, world!!! æ»≥Á«œººø‰.");
+			log.debug("debug : " + "Hello, world!!! æ»≥Á«œººø‰.");
+			log.info ("info  : " + "Hello, world!!! æ»≥Á«œººø‰.");
+			log.warn ("warn  : " + "Hello, world!!! æ»≥Á«œººø‰.");
+			log.error("error : " + "Hello, world!!! æ»≥Á«œººø‰.");
+			log.fatal("fatal : " + "Hello, world!!! æ»≥Á«œººø‰.");
 		}
 	}
 }
