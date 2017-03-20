0e144129692088450668aff96aebf255937e560b
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-20 10:59:34 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/commons/net/v01/smtp/SMTPCommand.java
+++ KIEA_JAVA7/src/tain/kr/com/commons/net/v01/smtp/SMTPCommand.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.commons.net.smtp;
+package tain.kr.com.commons.net.v01.smtp;
 
 /**
  * SMTPCommand stores a set of constants for SMTP command codes.  To interpret
