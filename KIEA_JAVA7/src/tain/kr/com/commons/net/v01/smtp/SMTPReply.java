222d99a9b2209651af526e9caf382268110ca533
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-20 10:59:41 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/commons/net/v01/smtp/SMTPReply.java
+++ KIEA_JAVA7/src/tain/kr/com/commons/net/v01/smtp/SMTPReply.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.commons.net.smtp;
+package tain.kr.com.commons.net.v01.smtp;
 
 /***
  * SMTPReply stores a set of constants for SMTP reply codes.  To interpret
