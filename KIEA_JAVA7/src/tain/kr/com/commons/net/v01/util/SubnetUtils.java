c9aaa2dc7922e0684daf343ff8213de92f73b5fd
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-20 11:03:16 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/commons/net/v01/util/SubnetUtils.java
+++ KIEA_JAVA7/src/tain/kr/com/commons/net/v01/util/SubnetUtils.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.commons.net.util;
+package tain.kr.com.commons.net.v01.util;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
