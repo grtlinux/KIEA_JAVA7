05523c6cf7f2a11655fd74c132aac75c47a48d24
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-20 10:56:35 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/commons/net/v01/ntp/TimeInfo.java
+++ KIEA_JAVA7/src/tain/kr/com/commons/net/v01/ntp/TimeInfo.java
@@ -1,4 +1,4 @@
-package org.apache.commons.net.ntp;
+package tain.kr.com.commons.net.v01.ntp;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
