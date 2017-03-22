b069ca940cff72ef255643df60c0c964edcfc862
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-22 23:03:38 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/junit/v03/ImpRequest.java
+++ KIEA_JAVA7/src/tain/kr/com/test/junit/v03/ImpRequest.java
@@ -19,7 +19,6 @@
  */
 package tain.kr.com.test.junit.v03;
 
-import org.apache.log4j.Logger;
 
 /**
  * Code Templates > Comments > Types
@@ -35,5 +34,7 @@ import org.apache.log4j.Logger;
  * @author taincokr
  *
  */
-public class ImpRequest {
-;
+public interface ImpRequest {
+	
+	public abstract String getName();
+}
