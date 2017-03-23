5d0aeca18dfd527593013d1048b9968bd392367c
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-24 01:58:41 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/junit/v03/test/ControllerDefaultTest04.java
+++ KIEA_JAVA7/src/tain/kr/com/test/junit/v03/test/ControllerDefaultTest04.java
@@ -19,12 +19,18 @@
  */
 package tain.kr.com.test.junit.v03.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import tain.kr.com.test.junit.v03.ControllerDefault;
 import tain.kr.com.test.junit.v03.ImpRequest;
 import tain.kr.com.test.junit.v03.ImpRequestHandler;
 import tain.kr.com.test.junit.v03.ImpResponse;
+import tain.kr.com.test.junit.v03.ResponseError;
 
 
 /**
@@ -45,13 +51,24 @@ public final class ControllerDefaultTest04 {
 
 	private final class RequestTest implements ImpRequest {
 
+		private static final String DEFAULT_NAME = "Test";
+		private String name;
+		
+		public RequestTest(String name) {
+			this.name = name;
+		}
+		
+		public RequestTest() {
+			this(DEFAULT_NAME);
+		}
+		
 		/* (non-Javadoc)
 		 * @see tain.kr.com.test.junit.v03.ImpRequest#getName()
 		 */
 		@Override
 		public String getName() {
 			// TODO Auto-generated method stub
-			return null;
+			return this.name;
 		}
 		
 	}
@@ -64,22 +81,39 @@ public final class ControllerDefaultTest04 {
 		@Override
 		public ImpResponse process(ImpRequest request) throws Exception {
 			// TODO Auto-generated method stub
-			return null;
+			return new ResponseTest();
 		}
 		
 	}
 	
 	private final class ResponseTest implements ImpResponse {
 
+		private static final String name = "Test";
+		
 		/* (non-Javadoc)
 		 * @see tain.kr.com.test.junit.v03.ImpResponse#getName()
 		 */
+		@SuppressWarnings("static-access")
 		@Override
 		public String getName() {
 			// TODO Auto-generated method stub
-			return null;
+			return this.name;
 		}
 		
+		public boolean equals(Object object) {
+			
+			boolean result = false;
+			
+			if (object instanceof ResponseTest) {
+				result = ((ResponseTest) object).getName().equals(this.getName());
+			}
+			
+			return result;
+		}
+		
+		public int hashCode() {
+			return name.hashCode();
+		}
 	}
 	
 	private final class ExceptionHandler implements ImpRequestHandler {
@@ -90,7 +124,7 @@ public final class ControllerDefaultTest04 {
 		@Override
 		public ImpResponse process(ImpRequest request) throws Exception {
 			// TODO Auto-generated method stub
-			return null;
+			throw new Exception("error processing request.");
 		}
 		
 	}
@@ -108,33 +142,63 @@ public final class ControllerDefaultTest04 {
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
+	private ControllerDefault controller;
+	private ImpRequest request;
+	private ImpRequestHandler handler;
+	
 	@Before
 	public void initialize() throws Exception {
 		
+		this.controller = new ControllerDefault();
+		this.request = new RequestTest();
+		this.handler = new HandlerRequestTest();
+		this.controller.addHandler(this.request, this.handler);
 	}
 	
 	@Test
 	public void testAddHandler() {
 		
+		ImpRequestHandler handler2 = this.controller.getHandler(this.request);
+		assertSame("Handler we get in controller should be the same handler we get.", handler2, this.handler);
 	}
 	
 	@Test
 	public void testProcessResponse() {
 		
+		ImpResponse response = this.controller.getResponse(this.request);
+		
+		assertNotNull("Must not return a null response", response);
+		assertEquals("response shuold be of type of ResponseTest", ResponseTest.class, response.getClass());
 	}
 	
 	@Test
 	public void testProcessRequestAnswersErrorResponse() {
 		
+		RequestTest request = new RequestTest("testError");
+		ExceptionHandler handler = new ExceptionHandler();
+		this.controller.addHandler(request, handler);
+		ImpResponse response = this.controller.getResponse(request);
+		
+		assertNotNull("Must not return a null response", response);
+		assertEquals("response", ResponseError.class, response.getClass());
 	}
 	
 	@Test ( expected = RuntimeException.class)
 	public void testGetHandlerNotDefined() {
 		
+		RequestTest request = new RequestTest("testNotDefined");
+		
+		// occured the event RuntimeException
+		this.controller.getHandler(request);
 	}
 	
 	@Test ( expected = RuntimeException.class)
 	public void testAddRequestDuplicateName() {
 		
+		RequestTest request = new RequestTest();
+		HandlerRequestTest handler = new HandlerRequestTest();
+		
+		// occured the event RuntimeException
+		this.controller.addHandler(request, handler);
 	}
 }
