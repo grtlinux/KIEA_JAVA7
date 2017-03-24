7e315ec3cf5b2574c184eb2c592e727f5c91baf5
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-24 18:37:04 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/github/hamcrestJson/v01/JSONAssertComparatorFactory.java
+++ KIEA_JAVA7/src/tain/kr/com/github/hamcrestJson/v01/JSONAssertComparatorFactory.java
@@ -1,5 +1,7 @@
 package tain.kr.com.github.hamcrestJson.v01;
 
+import tain.kr.com.github.json.JSONassert.v01.skyscreamer.jsonassert.JSONCompareMode;
+
 
 interface JSONAssertComparatorFactory<T> {
 	JSONComparator<T> comparatorWith(JSONCompareMode compareMode);
