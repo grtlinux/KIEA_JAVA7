0faafc5d2837e78d9f23b38dfaa27e07fa4d2f74
Kiea Seok Kang <grtlinux@gmail.com>
2017-03-31 06:13:28 +0900

    .

--- KIEA_JAVA7/Scripts/Script_firstdb.sql
+++ KIEA_JAVA7/Scripts/Script_firstdb.sql
@@ -1,6 +1,42 @@
+--
+-- database : firstdb
+--
+
 SELECT * FROM APP.FIRSTTABLE;
 
 SELECT DATE(TIMESTAMP('2000-01-01 12:12:00.000')) AS TIME_STAMP FROM SYSIBM.SYSDUMMY1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
