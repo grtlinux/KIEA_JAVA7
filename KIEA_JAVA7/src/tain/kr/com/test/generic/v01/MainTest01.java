89b7e0871f0943fdb99f675d799184f32c5e89e1
Kiea Seok Kang <grtlinux@gmail.com>
2017-04-23 22:24:02 +0900

    .

--- KIEA_JAVA7/src/tain/kr/com/test/generic/v01/MainTest01.java
+++ KIEA_JAVA7/src/tain/kr/com/test/generic/v01/MainTest01.java
@@ -44,14 +44,96 @@ public class MainTest01 {
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 
-	/*
-	 * constructor
-	 */
-	public MainTest01() {
-		if (flag)
-			log.debug(">>>>> in class " + this.getClass().getSimpleName());
+	@SuppressWarnings("unused")
+	private static class Generic<T> {
+		T radius;
+		T area;
+		public double get(T radius) {
+			double r = (double) radius;
+			double val = 2 * 3.14 * r;
+			return val;
+		}
 	}
 
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static class Box<T> {
+		// T stands for "Type"
+		private T t;
+		
+		public void set(T t) {
+			this.t = t;
+		}
+		
+		public T get() {
+			return t;
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static interface Pair<K, V> {
+		public K getKey();
+		public V getValue();
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	private static class Util {
+		
+		public static <K,V> boolean compare(Pair<K,V> p1, Pair<K,V> p2) {
+			return p1.getKey().equals(p2.getKey()) && p1.getValue().equals(p2.getValue());
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+
+	@SuppressWarnings("unused")
+	private static class OrderedPair<K,V> implements Pair<K,V> {
+		private K key;
+		private V value;
+		
+		public OrderedPair() {
+			this.key = null;
+			this.value = null;
+		}
+		
+		public OrderedPair(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+		
+		public void setKey(K key) {
+			this.key = key;
+		}
+		
+		public void setValue(V value) {
+			this.value = value;
+		}
+		
+		@Override
+		public K getKey() {
+			return this.key;
+		}
+		
+		@Override
+		public V getValue() {
+			return this.value;
+		}
+		
+		public static <K,V> boolean compare(Pair<K,V> p1, Pair<K,V> p2) {
+			return p1.getKey().equals(p2.getKey()) && p1.getValue().equals(p2.getValue());
+		}
+	}
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -68,13 +150,32 @@ public class MainTest01 {
 	/*
 	 * static test method
 	 */
+	@SuppressWarnings("unused")
 	private static void test01(String[] args) throws Exception {
 
-		if (flag)
-			new MainTest01();
-
 		if (flag) {
-
+			/*
+			 * begin
+			 */
+			Box<Integer> box = new Box<Integer>();
+			box.set(10);
+			box.get();
+			
+			// Pair<String, String> pair0 = new OrderedPair<String, String>();
+			
+			Pair<String, Integer> pair1 = new OrderedPair<String, Integer>(null, 0);
+			
+			Pair<String, Integer> pair2 = new OrderedPair<String, Integer>("Hello", 23);
+			
+			Pair<String, Integer> pair3 = new OrderedPair<>("Hello", 48);  // be equal to or more than jdk1.7
+			
+			Pair<Integer, String> p1 = new OrderedPair<Integer, String>(1, "apple");
+			
+			Pair<Integer, String> p2 = new OrderedPair<Integer, String>(3, "peer");
+			
+			boolean isSame = Util.<Integer,String>compare(p1, p2);
+			
+			System.out.printf("isSame = [%s]\n", isSame);
 		}
 	}
 
@@ -91,3 +192,27 @@ public class MainTest01 {
 			test01(args);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
